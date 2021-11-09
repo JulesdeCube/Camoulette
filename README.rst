e0acc9b3d6238696e8837764792be2d2add1243c
Jules Lefebvre <jules.lefebvre@epita.fr>
2021-11-09 10:44:28 +0100

    ADD: readme and update setup.py for pypi publish

---
+++ README.rst
@@ -0,0 +1,13 @@
+# Camoulette
+
+[
+![PyPI Version](https://img.shields.io/pypi/v/camoulette.svg)
+](https://pypi.org/project/camoulette/)
+[
+    ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/camoulette)
+](https://pypi.org/project/camoulette/)
+[
+    ![Downloads](https://pepy.tech/badge/camoulette)
+](https://pepy.tech/project/pynytimes)
+
+camoullette is a python libraray to grade studient. It's a class base test suite.
--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='camoulette', version='1.0', packages=find_packages())
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="camoulette",
+    version="1.0.0",
+    author="Jules Lefebvre",
+    author_email="jules.lefebvre@epita.fr",
+    description="A python class base test suite package to automate ocaml "
+                "practice and final grading",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JulesdeCube/Camoulette",
+    project_urls={
+        "Bug Tracker": "https://github.com/JulesdeCube/Camoulette/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.10",
+)
